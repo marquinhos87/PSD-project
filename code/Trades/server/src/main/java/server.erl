560159cf73cef80068cbaa4830d217d28f3bfabb
João Marques <joaomarques@Marquess-MBP.lan>
2020-01-09 23:55:38 +0000

    Add server (erlang) to project trades

---
+++ code/Trades/server/src/main/java/server.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author albertofaria joaomarques nunorei
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. jan 2020 23:53
+%%%-------------------------------------------------------------------
+-module(server).
+-author("albertofaria").
+-author("joaomarques").
+-author("nunorei").
+
+%% API
+-export([]).
