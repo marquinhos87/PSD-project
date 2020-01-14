0bbe720b0fb7ea25ec74b830ad08e677a65607d7
João Marques <joaomarques@Marquess-MacBook-Pro.local>
2020-01-14 17:17:47 +0000

    .

--- nefit/arbiter/src/psd/arbiter/Main.java
+++ nefit/arbiter/src/psd/arbiter/Main.java
@@ -2,6 +2,8 @@ package psd.arbiter;
 
 public class Main
 {
+    //List<Negociation> negociations;
+
     public static void main(String[] args)
     {
         System.out.println("Hello from the Java arbiter.");
