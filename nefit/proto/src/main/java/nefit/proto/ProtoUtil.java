5396a305cf7a0bccb5899a70392bd66ccd42f319
Alberto Faria <albertofaria.dev@gmail.com>
2020-01-19 01:58:41 +0000

    Informative commit message.

--- nefit/proto/src/main/java/nefit/proto/ProtoUtil.java
+++ nefit/proto/src/main/java/nefit/proto/ProtoUtil.java
@@ -8,13 +8,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
-public class ProtoUtil {
-
+public class ProtoUtil
+{
     private ProtoUtil()
     {
     }
 
-    public static <T> T read(InputStream in, Parser<T> msgParser) throws IOException {
+    public static < T > T read(InputStream in, Parser< T > msgParser)
+        throws IOException
+    {
         final var header = in.readNBytes(4);
         assert header.length == 4;
 
@@ -26,12 +28,13 @@ public class ProtoUtil {
         return msgParser.parseFrom(bytes);
     }
 
-    public static void write(OutputStream out, MessageLite msg) throws IOException {
+    public static void write(OutputStream out, MessageLite msg)
+        throws IOException
+    {
         final var msgSize = msg.getSerializedSize();
         final var header = ByteBuffer.allocate(4).putInt(msgSize).array();
 
         out.write(header);
         msg.writeTo(out);
     }
-
 }
