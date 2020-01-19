f86c26cf86585a786b46e60b09c1c5b6a2247cd5
Alberto Faria <albertofaria.dev@gmail.com>
2020-01-19 05:59:49 +0000

    Informative commit message.

--- nefit/client/src/main/java/nefit/client/Main.java
+++ nefit/client/src/main/java/nefit/client/Main.java
@@ -8,6 +8,18 @@ import java.net.InetSocketAddress;
 
 public class Main
 {
+    public static void main(String[] args)
+        throws IOException, InterruptedException
+    {
+        try (final var prompt = new Prompt())
+        {
+            final var serverEndpoint = parseArgs(prompt, args);
+            final var connection = connectToServer(prompt, serverEndpoint);
+
+            authenticateClient(prompt, connection);
+        }
+    }
+
     private static InetSocketAddress parseArgs(Prompt prompt, String[] args)
     {
         try
@@ -39,18 +51,6 @@ public class Main
         }
     }
 
-    public static void main(String[] args)
-        throws IOException, InterruptedException
-    {
-        try (final var prompt = new Prompt())
-        {
-            final var serverEndpoint = parseArgs(prompt, args);
-            final var connection = connectToServer(prompt, serverEndpoint);
-
-            authenticateClient(prompt, connection);
-        }
-    }
-
     private static void authenticateClient(
         Prompt prompt, Connection connection
     ) throws IOException, InterruptedException
@@ -72,27 +72,29 @@ public class Main
                 return;
         }
 
-        final var authMsg = connection.receive(
-            NefitProtos.ServerToClientAuth.parser()
-        );
+        // TODO: use newer messages
 
-        if (!authMsg.getOk())
-            prompt.fail(authMsg.getErrorMessage());
-
-        switch (authMsg.getClientType())
-        {
-            case IMPORTER:
-                prompt.print("You are now authenticated as an Importer.");
-                new Importer(prompt, connection, username).run();
-                break;
-
-            case MANUFACTURER:
-                prompt.print(
-                    "You are now authenticated as an Manufacturer."
-                );
-                new Manufacturer(prompt, connection, username).run();
-                break;
-        }
+//        final var authMsg = connection.receive(
+//            NefitProtos.ServerToClientAuth.parser()
+//        );
+//
+//        if (!authMsg.getOk())
+//            prompt.fail(authMsg.getErrorMessage());
+//
+//        switch (authMsg.getClientType())
+//        {
+//            case IMPORTER:
+//                prompt.print("You are now authenticated as an Importer.");
+//                new Importer(prompt, connection, username).run();
+//                break;
+//
+//            case MANUFACTURER:
+//                prompt.print(
+//                    "You are now authenticated as an Manufacturer."
+//                );
+//                new Manufacturer(prompt, connection, username).run();
+//                break;
+//        }
     }
 
     private static String login(Connection connection, Prompt prompt)
@@ -185,72 +187,4 @@ public class Main
 
         return username;
     }
-
-//    private static Function< Connection, Client > promptClientType(
-//        String[] args,
-//        Prompt prompt
-//    ) throws IOException {
-//
-//        final String str;
-//
-//        if (args.length >= 3)
-//            str = args[2];
-//        else
-//            str = prompt.input("Importer or Manufacturer [i/m]: ");
-//
-//        switch (str) {
-//            case "i":
-//                return null;
-//            case "m":
-//                return null;
-//            default:
-//                prompt.fail("Invalid client type.");
-//                return null;
-//        }
-//    }
-//
-//    private static NefitProtos.MsgAuth.MsgType promptAuthOp(
-//        String[] args,
-//        Prompt prompt
-//    ) throws IOException {
-//
-//        final String str;
-//
-//        if (args.length >= 4)
-//            str = args[3];
-//        else
-//            str = prompt.input("Login or Register [l/r]: ");
-//
-//        switch (str) {
-//            case "l":
-//                return NefitProtos.MsgAuth.MsgType.LOGIN;
-//            case "r":
-//                return NefitProtos.MsgAuth.MsgType.REGISTER;
-//            default:
-//                prompt.fail("Invalid authentication operation.");
-//                return null;
-//        }
-//    }
-//
-//    private static String promptUsername(
-//        String[] args,
-//        Prompt prompt
-//    ) throws IOException {
-//
-//        if (args.length >= 5)
-//            return args[4];
-//        else
-//            return prompt.input("Username: ");
-//    }
-//
-//    private static String promptPassword(
-//        String[] args,
-//        Prompt prompt
-//    ) throws IOException {
-//
-//        if (args.length >= 6)
-//            return args[5];
-//        else
-//            return prompt.input("Password: ");
-//    }
 }
