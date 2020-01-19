5c66e9ea841eccfed64e95b2e22066538bed0ba0
Alberto Faria <albertofaria.dev@gmail.com>
2020-01-19 23:57:53 +0000

    Informative commit message.

--- entrega/a79077+a81826+a81918/nefit/frontend/protoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-../rebar3 get-deps
-escript _build/default/plugins/gpb/bin/protoc-erl -modsuffix proto -o src/ ../shared/nefit.proto
--- entrega/a79077+a81826+a81918/nefit/shared/protoc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-protoc -I=. --java_out=src/main/java nefit.proto
